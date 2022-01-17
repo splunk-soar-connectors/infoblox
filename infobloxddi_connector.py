b8f67cab0958501e5d28bbf6d14b1a770c908fd4
knawattranakul <knawattranakul@splunk.com>
2022-01-17 13:24:24 -0800

    Fix SemGrep

--- infobloxddi_connector.py
+++ infobloxddi_connector.py
@@ -404,7 +404,7 @@ class InfobloxddiConnector(BaseConnector):
             consts.INFOBLOX_ERR_FROM_SERVER.format(status=request_obj.status_code, detail=consts.INFOBLOX_REST_RESP_OTHER_ERR_MSG)
         ), response_data
 
-    def _make_paged_rest_call(self, endpoint, action_result, params={}, **rest_call_options):
+    def _make_paged_rest_call(self, endpoint, action_result, params, **rest_call_options):
         """ Function used to make rest call requests in a paged fashion.
         This will alleviate errors with getting >1000 results from Infoblox.
 
@@ -420,6 +420,8 @@ class InfobloxddiConnector(BaseConnector):
         page_count = 1
         self.debug_print(consts.INFOBLOX_PAGE_COUNT.format(page_count))
 
+        if not params:
+            params = {}
         params[consts.INFOBLOX_JSON_PAGING] = 1
         params[consts.INFOBLOX_JSON_RETURN_AS_OBJECT] = 1
         params[consts.INFOBLOX_JSON_MAX_RESULTS] = 1000
@@ -1206,7 +1208,7 @@ if __name__ == "__main__":
     pudb.set_trace()
     if len(sys.argv) < 2:
         print("No test json specified as input")
-        exit(0)
+        sys.exit(0)
     with open(sys.argv[1]) as f:
         in_json = f.read()
         in_json = json.loads(in_json)
@@ -1215,4 +1217,4 @@ if __name__ == "__main__":
         connector.print_progress_message = True
         return_value = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(return_value), indent=4))
-    exit(0)
+    sys.exit(0)
